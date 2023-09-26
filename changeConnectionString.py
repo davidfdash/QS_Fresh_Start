f160bdcd0c578e5861107f67e952a4e76e063c31
David Fuller <davestoneyfuller@gmail.com>
2023-09-26 09:02:55 -0400

    resolved issues - It works now
    Added json.dump() to properly encode json data for put request.
    
    added encoding="utf-8-sig" to csv reader to get rid of leading garbage on dataconnection id

--- changeConnectionString.py
+++ changeConnectionString.py
@@ -13,28 +13,26 @@ headers = {'X-Qlik-xrfkey': xrf,
 #Set up the certificate path
 cert = 'C:\qscerts\clientandkey.pem'
 
-#create blank dict
-userAccessDates = {}
-
 #Set the endpoint URL
 xrfk = '?xrfkey={}'.format(xrf)
 endpoint = '/qrs/dataconnection/'
 #pull analyzers
-with open('connectionsToEdit.csv', newline='') as f:
+with open('connectionsToEdit.csv', newline='', encoding="utf-8-sig") as f:
     reader = csv.reader(f)
-    #for row in reader:
-        #print(row[2])
-        #id = row[0]
-    id = '6f2fc6e7-9d56-48a7-955c-dc1bbfa61e19'
-    url = QS_Node + endpoint + id + xrfk
-    #print(url)
-    response = requests.get(url, headers=headers, verify=False, cert=cert)
-    #print(response)
-    connection = json.loads(response.text)
-
-    connString = (connection['connectionstring'])
-    newConnString = connString.replace('C:', 'D:')
-    #print(newConnString)
-    connection['connectionstring'] = newConnString
-    #print(connection)
-    r = requests.put(url, headers=headers, verify=False, cert=cert, data=connection)
+    for row in reader:
+        #print(row[0])
+        id = row[0]
+        #id = '6f2fc6e7-9d56-48a7-955c-dc1bbfa61e19'
+        url = QS_Node + endpoint + id + xrfk
+        #print(url)
+        response = requests.get(url, headers=headers, verify=False, cert=cert)
+        print('Get request response: ' + str(response))
+        connection = json.loads(response.text)
+        connString = (connection['connectionstring'])
+        newConnString = connString.replace('C:', 'D:')
+        #print(newConnString)
+        connection['connectionstring'] = newConnString
+        #print(connection['connectionstring'])
+        #print(connection)
+        r = requests.put(url, headers=headers, verify=False, cert=cert, data=json.dumps(connection))
+        print('Put request response: ' + str(r))

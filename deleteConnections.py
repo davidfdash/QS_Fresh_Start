42c2b3d69aa84ddf617cbd63b5cb39d29d4bd9fc
David Fuller <davestoneyfuller@gmail.com>
2023-10-05 10:03:59 -0400

    Update deleteConnections.py

--- deleteConnections.py
+++ deleteConnections.py
@@ -19,7 +19,7 @@ userAccessDates = {}
 xrfk = '?xrfkey={}'.format(xrf)
 endpoint = '/qrs/dataconnection/'
 #pull analyzers
-with open('connectionsToDelete.csv', newline='') as f:
+with open('connectionsToDelete.csv', newline='', encoding="utf-8-sig") as f:
     reader = csv.reader(f)
     for row in reader:
         #print(row[2])

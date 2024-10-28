e9b8c9f5cde25fa3236e22a66209e22d942bc4a2
David Fuller <davestoneyfuller@gmail.com>
2024-10-28 14:29:13 -0400

    updates from VSCode

--- Variables.py
+++ Variables.py
@@ -1,4 +1,4 @@
 #Universal Variables
 
-QS_Node = 'https://qseowcentral:4242'
-user = "UserDirectory=test;UserId=svc_acct"
+QS_Node = 'https://ec2-54-210-217-253.compute-1.amazonaws.com:4242'
+user = "UserDirectory=AZURE;UserId=bobtheadmin@qlik752.onmicrosoft.com"

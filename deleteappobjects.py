d0aeb5de8fb1e018c9fdc86a3e59024c7a815b3d
David Fuller <davestoneyfuller@gmail.com>
2025-01-02 09:27:50 -0500

    add app object deleter

---
+++ deleteappobjects.py
@@ -0,0 +1,30 @@
+import requests
+requests.packages.urllib3.disable_warnings()
+import csv
+from Variables import user, QS_Node
+
+#Set up necessary headers comma separated
+xrf = 'davedavedavedave'
+headers = {'X-Qlik-xrfkey': xrf,
+"Content-Type": "application/json",
+"X-Qlik-User":user}
+
+#Set up the certificate path
+cert = 'C:\qscerts\clientandkey.pem'
+
+#create blank dict
+userAccessDates = {}
+
+#Set the endpoint URL
+xrfk = '?xrfkey={}'.format(xrf)
+endpoint = '/qrs/app/object/'
+#pull analyzers
+with open('objectsToDelete.csv', newline='', encoding="utf-8-sig") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        #print(row[2])
+        id = row[0]
+        url = QS_Node + endpoint + id + xrfk
+        #print(url)
+        lresp = requests.delete(url, headers=headers, verify=False, cert=cert)
+        print(lresp)

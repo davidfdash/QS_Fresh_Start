729f7eb0f16fdb0f55f5e33fb1c721ef1cbbeac3
David Fuller <davestoneyfuller@gmail.com>
2025-03-10 16:03:15 -0400

    add app object deleter

--- Variables.py
+++ Variables.py
@@ -1,4 +1,4 @@
 #Universal Variables
 
 QS_Node = 'https://ec2-54-210-217-253.compute-1.amazonaws.com:4242'
-user = "UserDirectory=AZURE;UserId=bobtheadmin@qlik752.onmicrosoft.com"
+user = "UserDirectory=INTERNAL;UserId=sa_repository"

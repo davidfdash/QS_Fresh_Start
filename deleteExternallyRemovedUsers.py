c68d4b46ce581706fdeed2b34a0bb99bb4d009e9
David Fuller <davestoneyfuller@gmail.com>
2025-10-28 10:28:28 -0400

    delicenser complete

---
+++ deleteExternallyRemovedUsers.py
@@ -0,0 +1,41 @@
+import requests
+import csv
+from Variables import user, QS_Node
+
+requests.packages.urllib3.disable_warnings()
+
+#Set up necessary headers comma separated
+xrf = 'iX83QmNlvu87yyAB'
+headers = {'X-Qlik-xrfkey': xrf,
+"Content-Type": "application/json",
+"X-Qlik-User":user}
+
+#Set up the certificate path
+cert = 'C:\\qscerts\\clientandkey.pem'
+
+#Set the endpoint URL
+endpoint = '/qrs/user/full'
+xrfk = '?xrfkey={}'.format(xrf)
+url = QS_Node + endpoint + xrfk
+#print(url)
+removedExternally = []
+resp = requests.get(url, headers=headers, verify=False, cert=cert)
+for i in resp.json():
+    #print(i['inactive'])
+    if i['removedExternally'] == True:
+        #print([i['userId'], i['inactive'], i['id']])
+        removedExternally.append([i['userId'], i['removedExternally'], i['id']])
+print(removedExternally)
+
+#Set the endpoint URL
+endpoint = '/qrs/user/'
+#pull analyzers
+
+for i in removedExternally:
+    #print(row[2])
+    id = i[2]
+    url = QS_Node + endpoint + id + xrfk
+    #print(url)
+    lresp = requests.delete(url, headers=headers, verify=False, cert=cert)
+    print(lresp)
+
